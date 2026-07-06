c36d8310e4d0517ec7861a3537c00e4260b0eb85
yilmazhalilibrahim891-crypto <yilmazhalilibrahim891@gmail.com>
2026-07-06 09:44:17 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+});
